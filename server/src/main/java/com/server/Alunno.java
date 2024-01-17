ca3c33854848e66b294d2aa4856415e0083c7875
lai2-24 <lai2-24@itismeucci.com>
2024-01-17 13:43:47 +0100

    added .gitignore for both server and client

---
+++ server/src/main/java/com/server/Alunno.java
@@ -0,0 +1,34 @@
+package com.server;
+
+public class Alunno {
+    private String nome;
+    private String cognome;
+    int eta;
+
+    public Alunno(){
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public int getEta() {
+        return eta;
+    }
+
+    public void setEta(int eta) {
+        this.eta = eta;
+    }
+}
